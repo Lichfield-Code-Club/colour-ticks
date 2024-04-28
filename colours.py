e230227a5b7ad4457b5599fd7841db4f497bc8ff
Mark Pritchett <unixmiddleware@gmail.com>
2024-04-28 10:54:39 +0100

    Corrected typos

--- colours.py
+++ colours.py
@@ -1,15 +1,15 @@
 import pygame
 from colorsys import hsv_to_rgb
     
-def get_rgb_color_list(num_colors=5, saturation=1.0, value=1.0):
-    rgb_colors = []
-    hsv_colors = [[float(x / num_colors), saturation, value] for x in range(num_colors)]
+def get_rgb_colour_list(num_colours=5, saturation=1.0, value=1.0):
+    rgb_colours = []
+    hsv_colours = [[float(x / num_colours), saturation, value] for x in range(num_colours)]
     
-    for hsv in hsv_colors:
+    for hsv in hsv_colours:
        hsv = [int(x * 255) for x in hsv_to_rgb(*hsv)]
-       rgb_colors.append(hsv)
+       rgb_colours.append(hsv)
     
-    return rgb_colors
+    return rgb_colours
 
 # Initialize Pygame
 pygame.init()
@@ -17,11 +17,11 @@ pygame.init()
 # Set up the display
 screen = pygame.display.set_mode((400, 300))
 
-colour_list = get_rgb_color_list(num_colors=60)
+colour_list = get_rgb_colour_list(num_colours=60)
 colour_list_index = 0
 
 # Time variables
-change_color_every_ms = 200  # Total time to change from one color to the next
+change_colour_every_ms = 200  # Total time to change from one colour to the next
 last_change_time = pygame.time.get_ticks()
 increment = 1
 
@@ -35,7 +35,7 @@ while running:
     # Get the current time
     current_time = pygame.time.get_ticks()
 
-    if (current_time - last_change_time) > change_color_every_ms:
+    if (current_time - last_change_time) > change_colour_every_ms:
         last_change_time = current_time
         colour_list_index += increment
         if colour_list_index > len(colour_list) - 1:
@@ -48,7 +48,7 @@ while running:
     rgb_colour = colour_list[colour_list_index]
 
 
-    # Fill the screen with the interpolated color
+    # Fill the screen with the interpolated colour
     screen.fill(rgb_colour)
     pygame.display.flip()
 
